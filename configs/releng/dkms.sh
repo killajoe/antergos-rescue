a17fe19910be2837fae88cee0d6b498443ff5987
Johannes Kamprad <archlinux@kamprad.net>
2018-01-28 15:59:08 +0100

    Create dkms.sh

---
+++ configs/releng/dkms.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+_KERNVER_STR="$(pacman -Q linux)"
+_KERNVER="${_KERNVER_STR/linux }-ARCH"
+_MODVER_STR="$(pacman -Q zfs)"
+_MODVER_STR="${_MODVER_STR/zfs }"
+_MODVER="${_MODVER_STR%-*}"
+
+pacman -S --needed linux-headers
+
+echo '>>> Updating module dependencies. Please wait ...'
+
+if [[ $(dkms status -k "${_KERNVER}" "spl/${_MODVER}") != *'installed'* ]]; then
+	{ dkms install -k "${_KERNVER}" "spl/${_MODVER}"; } || exit 1
+fi
+
+if [[ $(dkms status -k "${_KERNVER}" "zfs/${_MODVER}") != *'installed'* ]]; then
+	{ dkms install -k "${_KERNVER}" "zfs/${_MODVER}"; } || exit 1
+fi
+
+{ mkinitcpio -p linux && exit 0; } || exit 1
